d3dac36afc613a56e09cb912be1edd546d0b6e62
Majd212003 <majdmajood0035@gmail.com>
2026-05-12 16:47:30 +0300

    project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // 1. التحقق من البيانات (الخط الدفاعي الأول ضد التضارب)
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'phone_number'=> 'sometimes|string|min:10|max:14|unique:users,phone_number',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // 2. إنشاء المستخدم
+        // ملاحظة: قاعدة البيانات ستحمينا أيضاً بفضل قيد الـ Unique على الإيميل
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+
+
+        return response()->json([
+            'message' => 'User registered successfully' ,
+            'token' => $token,
+        ], 201);
+    }
+
+    /**
+     * تسجيل الدخول
+     */
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    /**
+     * تسجيل الخروج
+     */
+    public function logout(Request $request)
+    {
+        // حذف التوكن الحالي لإنهاء الجلسة
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
